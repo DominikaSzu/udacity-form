e5547517ac028aca7e1fbea67a0a33560b94b5fc
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-05 13:06:28 +0200

    Progress bar implementation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
 
+    const generalInfo = document.querySelector('.general-info');
+    console.log(generalInfo);    
+    const shippingAddress = document.querySelector('.shipping-address');
+    console.log(shippingAddress);    
+    const paymentInfo = document.querySelector('.payment-info');
+    console.log(paymentInfo);    
+    const billingAddress = document.querySelector('.billing-address');
+    console.log(billingAddress);
+    let progressBar = document.querySelector('#progress');
+    console.log(progressBar)
+    
+    generalInfo.addEventListener('keypress', function() {
+        progressBar.setAttribute('value', '25');
+    })
+    
+    shippingAddress.addEventListener('keypress', function() {
+        progressBar.setAttribute('value', '50');
+    })
+    
+    paymentInfo.addEventListener('keypress', function() {
+        progressBar.setAttribute('value', '75');
+    })
+    
+    billingAddress.addEventListener('keypress', function() {
+        progressBar.setAttribute('value', '100');
+    })
+    
+    
 });
