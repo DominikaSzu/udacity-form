d4c4bb4955a45b3aa47d194a0aff0b2aedc31c8f
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-05 15:04:09 +0200

    Progress bar shows progress

--- js/app.js
+++ js/app.js
@@ -1,30 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const generalInfo = document.querySelector('.general-info');
-    console.log(generalInfo);    
-    const shippingAddress = document.querySelector('.shipping-address');
-    console.log(shippingAddress);    
-    const paymentInfo = document.querySelector('.payment-info');
-    console.log(paymentInfo);    
-    const billingAddress = document.querySelector('.billing-address');
-    console.log(billingAddress);
-    let progressBar = document.querySelector('#progress');
-    console.log(progressBar)
+    const generalInfo = document.querySelector('.general-info'); const shippingAddress = document.querySelector('.shipping-address'); 
+    const paymentInfo = document.querySelector('.payment-info'); const billingAddress = document.querySelector('.billing-address');
+    let progressBar = document.querySelector('#progress');  
+    let submitButton = document.querySelector('#submit');
+    
+//    Event listeners to mark the progress on progress bar
     
     generalInfo.addEventListener('keypress', function() {
         progressBar.setAttribute('value', '25');
-    })
+    });
     
     shippingAddress.addEventListener('keypress', function() {
         progressBar.setAttribute('value', '50');
-    })
+    });
     
     paymentInfo.addEventListener('keypress', function() {
         progressBar.setAttribute('value', '75');
-    })
+    });
     
     billingAddress.addEventListener('keypress', function() {
         progressBar.setAttribute('value', '100');
+    });
+    
+    
+//    Validation of inserted data
+    
+    let allTextInputs = Array.from(document.getElementsByClassName('text-input'));
+    console.log(allTextInputs)    
+    
+    submitButton.addEventListener('click', function() {
+        allTextInputs.forEach((textInput) => {
+            console.log(textInput.value)
+
+    })
     })
     
     
